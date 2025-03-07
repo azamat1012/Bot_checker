96ab2f7ff5943a5bd3aba22d34c91b235ddd6115
azamat1012 <148556590+azamat1012@users.noreply.github.com>
2025-03-07 14:12:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,106 +1,89 @@
 import requests
-import os
 from time import sleep
 import logging
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
+import environ
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
-
-load_dotenv()
-DEVMAN_API = os.getenv("DEVMAN_API")
-TG_API = os.getenv("TG_API")
-
-if not DEVMAN_API:
-    logging.error(
-        "DEVMAN_API не был найден. Пожалуйста, напишите DEVMAN_API в .env")
-    exit(1)
-if not TG_API:
-    logging.error(
-        "TG_API токен не был найден. Пожалуйста, напишите TG_API в .env")
-    exit(1)
+logger = logging.getLogger(__name__)
 
 
-def fetch_reviews(url, token, params=None):
-    headers = {"Authorization": f"Token {token}"}
-    try:
-        response = requests.get(url, headers=headers,
-                                params=params, timeout=10)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.HTTPError as error:
-        logging.error(f"HTTP Error: {error}")
-        raise
-    except requests.exceptions.ReadTimeout as error:
-        logging.error(f"Read Timeout: {error}")
-        raise
-    except requests.exceptions.ConnectionError as error:
-        logging.error(f"Connection Error: {error}")
-        raise
-
-
-def search_changes(context: CallbackContext):
+def get_checks(context: CallbackContext):
     URL = "https://dvmn.org/api/long_polling/"
     params = {}
     chat_id = context.job.context
 
     while True:
         try:
-            data = fetch_reviews(URL, DEVMAN_API, params)
-            logging.debug(f"Response: {data}")
-            if data["status"] == "found":
-                logging.info(f"Новая попытка: {data['new_attempts']}")
-                for attempt in data["new_attempts"]:
+            response = requests.get(URL, DEVMAN_TOKEN, params)
+            response.raise_for_status()
+            fetched_checks = response.json()
+            logger.debug(f"Response: {fetched_checks}")
+            if fetched_checks["status"] == "found":
+                logger.info(f"Новая попытка: {fetched_checks['new_attempts']}")
+                for attempt in fetched_checks["new_attempts"]:
                     message = (
                         f"У Вас проверили работу «{attempt['lesson_title']}» {attempt['lesson_url']}\n\n"
-                        f"{'Преподавателю все понравилось, можно приступать к следующему уроку!' if not attempt['is_negative'] else 'К сожалению, в работе нашлись ошибки'}\n"
+                        f"{'Преподавателю все понравилось, можно приступать к следующему уроку!'
+                            if not attempt['is_negative'] else 'К сожалению, в работе нашлись ошибки'}\n"
                     )
-                    context.bot.send_message(
-                        chat_id=chat_id,
-                        text=message)
-                params["timestamp"] = data["last_attempt_timestamp"]
-            elif data["status"] == "timeout":
-                params["timestamp"] = data["timestamp_to_request"]
-        except (requests.exceptions.HTTPError, requests.exceptions.ReadTimeout,
+                    context.bot.send_message(chat_id=chat_id, text=message)
+                params["timestamp"] = fetched_checks["last_attempt_timestamp"]
+            elif fetched_checks["status"] == "timeout":
+                params["timestamp"] = fetched_checks["timestamp_to_request"]
+        except (requests.exceptions.HTTPError,
                 requests.exceptions.ConnectionError) as error:
-            logging.error(f"Error: {error}")
+            logger.error(f"Error: {error}")
             sleep(5)
             continue
         except Exception as e:
-            logging.error(f"Error: {e}")
+            logger.error(f"Error: {e}")
             sleep(5)
             continue
 
 
 def start(update: Update, context: CallbackContext):
     chat_id = update.message.chat_id
-    update.message.reply_text(
-        "Начинаю поиск новых проверок!")
+    update.message.reply_text("Начинаю поиск новых проверок!")
 
     if not context.chat_data.get("polling_started"):
         context.job_queue.run_repeating(
-            search_changes,
-            interval=1,
-            first=0,
-            context=chat_id
-        )
+            get_checks, interval=1, first=0, context=chat_id)
         context.chat_data["polling_started"] = True
     else:
         update.message.reply_text("Проверка уже запущена!")
 
 
 def main():
-    updater = Updater(TG_API, use_context=True)
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+    DEVMAN_TOKEN
+
+    if not DEVMAN_TOKEN:
+        logger.error(
+            "DEVMAN_API не был найден. Пожалуйста, напишите DEVMAN_API в .env")
+        exit(1)
+    if not TG_BOT_TOKEN:
+        logger.error(
+            "TG_BOT_TOKEN токен не был найден. Пожалуйста, напишите TG_BOT_TOKEN в .env")
+        exit(1)
+
+    updater = Updater(TG_BOT_TOKEN, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('start', start))
-    logging.info("Bot is starting")
+    logger.info("Bot is starting")
     updater.start_polling()
     updater.idle()
 
 
 if __name__ == "__main__":
+    env = environ.Env(
+        DEBUG=(bool, False)
+    )
+    environ.Env.read_env()
+    DEVMAN_TOKEN = env("DEVMAN_TOKEN")
+    TG_BOT_TOKEN = env("TG_BOT_TOKEN")
     main()
